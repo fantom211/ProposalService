89bb7dbe8f667847977b6a0f9186cf232aea111b
wave <wave@sosal.com>
2026-03-31 19:27:35 +0200

    Данёк, как ты мне надоел со своими миграциями, болван

--- ProposalService/Program.cs
+++ ProposalService/Program.cs
@@ -46,6 +46,12 @@ builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
