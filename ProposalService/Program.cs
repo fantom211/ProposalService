2dedc9b74a58f91f250823b4551c9f1e129a08c0
fantom211 <d.perepichka2905@gmail.com>
2026-03-31 21:03:07 +0200

    program update

--- ProposalService/Program.cs
+++ ProposalService/Program.cs
@@ -5,29 +5,28 @@ using WorkService.Exceptions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Регистрация сервисов
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = null;
+        options.JsonSerializerOptions.WriteIndented = true;
+        options.JsonSerializerOptions.Converters.Add(
+            new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Подключение к БД
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// DI для сервисов
 builder.Services.AddScoped<ServiceProposal>();
-
-builder.Services.AddControllers()
-.AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.ReferenceHandler = null;
-    options.JsonSerializerOptions.WriteIndented = true;
-    options.JsonSerializerOptions.Converters.Add(
-        new System.Text.Json.Serialization.JsonStringEnumConverter());
-});
-
 builder.Services.AddScoped<NotificationServiceClient>();
 builder.Services.AddScoped<WorkServiceClient>();
 
-
+// Настройка HttpClient для внешних сервисов
 builder.Services.AddHttpClient<NotificationServiceClient>((sp, client) =>
 {
     var config = sp.GetRequiredService<IConfiguration>();
@@ -40,24 +39,20 @@ builder.Services.AddHttpClient<WorkServiceClient>((sp, client) =>
     client.BaseAddress = new Uri(config["Services:WorkService"]);
 });
 
-
+// Глобальный обработчик исключений
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
 
+// Создание приложения
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
-
-// Configure the HTTP request pipeline.
+// Middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseExceptionHandler();
 app.UseStatusCodePages();
 app.UseHttpsRedirection();
