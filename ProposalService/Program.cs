917e89e5dde8e185c52e903a4ea1a51fbafb24ec
wave <wave@sosal.com>
2026-03-31 22:16:31 +0200

    aga

--- ProposalService/Program.cs
+++ ProposalService/Program.cs
@@ -55,6 +55,13 @@ builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
